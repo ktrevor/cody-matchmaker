a2bd354b48d4be2c7048c4f664696135c11f3b63
ktrevor <katestrevor@gmail.com>
2025-01-11 22:53:40 -0800

    fixed modal spin

--- src/pages/MemberPage/AddMember.tsx
+++ src/pages/MemberPage/AddMember.tsx
@@ -9,6 +9,7 @@ import {
   Select,
   Radio,
   message,
+  Spin,
 } from "antd";
 import { UserAddOutlined } from "@ant-design/icons";
 import { addMember } from "../../members/firebaseMemberFunctions";
@@ -55,7 +56,6 @@ export const AddMember = ({ updateMembers, members }: AddMemberProps) => {
   };
 
   const onFinish: FormProps<FieldType>["onFinish"] = async (newMember) => {
-    if (confirmLoading) return;
     setConfirmLoading(true);
     newMember.name = capitalizeName(newMember.name);
     if (newMember.tree === "None") {
@@ -85,119 +85,121 @@ export const AddMember = ({ updateMembers, members }: AddMemberProps) => {
         footer={null}
         closable={!confirmLoading}
       >
-        <Form
-          form={form}
-          name="basic"
-          onFinish={onFinish}
-          disabled={confirmLoading}
-          autoComplete="off"
-        >
-          <Form.Item<FieldType>
-            label="Name"
-            name="name"
-            rules={[{ required: true, message: "Name is required." }]}
+        <Spin spinning={confirmLoading}>
+          <Form
+            form={form}
+            name="basic"
+            onFinish={onFinish}
+            disabled={confirmLoading}
+            autoComplete="off"
           >
-            <Input />
-          </Form.Item>
+            <Form.Item<FieldType>
+              label="Name"
+              name="name"
+              rules={[{ required: true, message: "Name is required." }]}
+            >
+              <Input />
+            </Form.Item>
 
-          <Form.Item<FieldType>
-            label="Grade"
-            name="grade"
-            rules={[{ required: true, message: "Grade is required." }]}
-          >
-            <Select>
-              <Select.Option value="Freshman">Freshman</Select.Option>
-              <Select.Option value="Sophmore">Sophmore</Select.Option>
-              <Select.Option value="Junior">Junior</Select.Option>
-              <Select.Option value="Senior">Senior</Select.Option>
-              <Select.Option value="Super Senior">Super Senior</Select.Option>
-            </Select>
-          </Form.Item>
+            <Form.Item<FieldType>
+              label="Grade"
+              name="grade"
+              rules={[{ required: true, message: "Grade is required." }]}
+            >
+              <Select>
+                <Select.Option value="Freshman">Freshman</Select.Option>
+                <Select.Option value="Sophmore">Sophmore</Select.Option>
+                <Select.Option value="Junior">Junior</Select.Option>
+                <Select.Option value="Senior">Senior</Select.Option>
+                <Select.Option value="Super Senior">Super Senior</Select.Option>
+              </Select>
+            </Form.Item>
 
-          <Form.Item<FieldType>
-            label="Gender"
-            name="gender"
-            rules={[{ required: true, message: "Gender is required." }]}
-          >
-            <Radio.Group>
-              <Radio value="Male"> Male </Radio>
-              <Radio value="Female"> Female </Radio>
-              <Radio value="Non-binary"> Non-binary </Radio>
-              <Radio value="Other"> Other </Radio>
-            </Radio.Group>
-          </Form.Item>
+            <Form.Item<FieldType>
+              label="Gender"
+              name="gender"
+              rules={[{ required: true, message: "Gender is required." }]}
+            >
+              <Radio.Group>
+                <Radio value="Male"> Male </Radio>
+                <Radio value="Female"> Female </Radio>
+                <Radio value="Non-binary"> Non-binary </Radio>
+                <Radio value="Other"> Other </Radio>
+              </Radio.Group>
+            </Form.Item>
 
-          <Form.Item<FieldType>
-            label="Joined"
-            name="joined"
-            rules={[{ required: true, message: "Joined is required." }]}
-          >
-            <Select>
-              <Select.Option value="Spring 2025">Spring 2025</Select.Option>
-              <Select.Option value="Fall 2024">Fall 2024</Select.Option>
-              <Select.Option value="Spring 2024">Spring 2024</Select.Option>
-              <Select.Option value="Fall 2023">Fall 2023</Select.Option>
-              <Select.Option value="Spring 2023">Spring 2023</Select.Option>
-              <Select.Option value="Fall 2022">Fall 2022</Select.Option>
-              <Select.Option value="Spring 2022">Spring 2022</Select.Option>
-              <Select.Option value="Fall 2021">Fall 2021</Select.Option>
-            </Select>
-          </Form.Item>
+            <Form.Item<FieldType>
+              label="Joined"
+              name="joined"
+              rules={[{ required: true, message: "Joined is required." }]}
+            >
+              <Select>
+                <Select.Option value="Spring 2025">Spring 2025</Select.Option>
+                <Select.Option value="Fall 2024">Fall 2024</Select.Option>
+                <Select.Option value="Spring 2024">Spring 2024</Select.Option>
+                <Select.Option value="Fall 2023">Fall 2023</Select.Option>
+                <Select.Option value="Spring 2023">Spring 2023</Select.Option>
+                <Select.Option value="Fall 2022">Fall 2022</Select.Option>
+                <Select.Option value="Spring 2022">Spring 2022</Select.Option>
+                <Select.Option value="Fall 2021">Fall 2021</Select.Option>
+              </Select>
+            </Form.Item>
 
-          <Form.Item<FieldType>
-            label="Forest"
-            name="forest"
-            rules={[{ required: true, message: "Forest is required." }]}
-          >
-            <Select>
-              <Select.Option value="Lost In The Woods">
-                Lost In The Woods
-              </Select.Option>
-              <Select.Option value="Ragtag">Ragtag</Select.Option>
-              <Select.Option value="Howl's Moving Forest">
-                Howl's Moving Forest
-              </Select.Option>
-              <Select.Option value="Magic Tree House">
-                Magic Tree House
-              </Select.Option>
-              <Select.Option value="Onlyfamilia">Onlyfamilia</Select.Option>
-            </Select>
-          </Form.Item>
+            <Form.Item<FieldType>
+              label="Forest"
+              name="forest"
+              rules={[{ required: true, message: "Forest is required." }]}
+            >
+              <Select>
+                <Select.Option value="Lost In The Woods">
+                  Lost In The Woods
+                </Select.Option>
+                <Select.Option value="Ragtag">Ragtag</Select.Option>
+                <Select.Option value="Howl's Moving Forest">
+                  Howl's Moving Forest
+                </Select.Option>
+                <Select.Option value="Magic Tree House">
+                  Magic Tree House
+                </Select.Option>
+                <Select.Option value="Onlyfamilia">Onlyfamilia</Select.Option>
+              </Select>
+            </Form.Item>
 
-          <Form.Item<FieldType>
-            label="Tree"
-            name="tree"
-            rules={[{ required: true, message: "Tree is required." }]}
-          >
-            <Select
-              showSearch
-              filterOption={(input, option) =>
-                (option?.label ?? "")
-                  .toLowerCase()
-                  .includes(input.toLowerCase())
-              }
-              options={[
-                { value: "None", label: "None" },
-                ...members.map((member) => ({
-                  value: member.id,
-                  label: member.name,
-                })),
-              ]}
-              virtual
-            />
-          </Form.Item>
+            <Form.Item<FieldType>
+              label="Tree"
+              name="tree"
+              rules={[{ required: true, message: "Tree is required." }]}
+            >
+              <Select
+                showSearch
+                filterOption={(input, option) =>
+                  (option?.label ?? "")
+                    .toLowerCase()
+                    .includes(input.toLowerCase())
+                }
+                options={[
+                  { value: "None", label: "None" },
+                  ...members.map((member) => ({
+                    value: member.id,
+                    label: member.name,
+                  })),
+                ]}
+                virtual
+              />
+            </Form.Item>
 
-          <Form.Item {...tailLayout}>
-            <Space>
-              <Button htmlType="button" onClick={handleCancel}>
-                Cancel
-              </Button>
-              <Button type="primary" htmlType="submit" loading={confirmLoading}>
-                Submit
-              </Button>
-            </Space>
-          </Form.Item>
-        </Form>
+            <Form.Item {...tailLayout}>
+              <Space>
+                <Button htmlType="button" onClick={handleCancel}>
+                  Cancel
+                </Button>
+                <Button type="primary" htmlType="submit">
+                  Submit
+                </Button>
+              </Space>
+            </Form.Item>
+          </Form>
+        </Spin>
       </Modal>
     </>
   );
