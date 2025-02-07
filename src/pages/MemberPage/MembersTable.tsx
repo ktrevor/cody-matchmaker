4d3ef76257e54b14cfa4d11b402a2a7205adad25
ktrevor <katestrevor@gmail.com>
2025-02-06 21:48:16 -0800

    categorical filters done

--- src/pages/MemberPage/MembersTable.tsx
+++ src/pages/MemberPage/MembersTable.tsx
@@ -1,6 +1,6 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, Key } from "react";
 import { Table, Space, Modal, message, Button, List } from "antd";
-import { getNextGrade, Member } from "../../members/Member";
+import { Member } from "../../members/Member";
 import {
   deleteMember,
   promoteMembersGrades,
@@ -8,12 +8,11 @@ import {
 import { EditMember } from "./EditMember";
 import { useMembersContext } from "../../components/MembersProvider";
 import { DeleteOutlined } from "@ant-design/icons";
-import { TableRowSelection } from "antd/es/table/interface";
+import { ColumnsType } from "antd/es/table";
 
 export const MemberTable = () => {
   const { members, updateMembers } = useMembersContext();
   const [treeNames, setTreeNames] = useState<{ [key: string]: string }>({});
-  const [selectedMembers, setSelectedMembers] = useState<Member[]>([]);
 
   const [pagination, setPagination] = useState({
     current: 1,
@@ -85,7 +84,6 @@ export const MemberTable = () => {
       onOk: async () => {
         await promoteMembersGrades(members);
         updateMembers();
-        setSelectedMembers([]); //clear selection
         message.success("Members promoted successfully!");
       },
       okText: "Confirm",
@@ -94,7 +92,7 @@ export const MemberTable = () => {
     });
   };
 
-  const columns = [
+  const columns: ColumnsType<Member> = [
     {
       title: "Name",
       dataIndex: "name",
@@ -109,21 +107,62 @@ export const MemberTable = () => {
       title: "Grade",
       dataIndex: "grade",
       key: "grade",
+      filters: [
+        { text: "Freshman", value: "Freshman" },
+        { text: "Sophomore", value: "Sophomore" },
+        { text: "Junior", value: "Junior" },
+        { text: "Senior", value: "Senior" },
+      ],
+      onFilter: (value: boolean | Key, record: Member) => {
+        return record.grade.includes(value as string);
+      },
     },
     {
       title: "Gender",
       dataIndex: "gender",
       key: "gender",
+      filters: [
+        { text: "Male", value: "Male" },
+        { text: "Female", value: "Female" },
+        { text: "Non-binary", value: "Non-binary" },
+        { text: "Other", value: "Other" },
+      ],
+      onFilter: (value: boolean | Key, record: Member) => {
+        return record.gender.includes(value as string);
+      },
     },
     {
       title: "Joined",
       dataIndex: "joined",
       key: "joined",
+      filters: [
+        { text: "Spring 2025", value: "Spring 2025" },
+        { text: "Fall 2024", value: "Fall 2024" },
+        { text: "Spring 2024", value: "Spring 2024" },
+        { text: "Fall 2023", value: "Fall 2023" },
+        { text: "Spring 2023", value: "Spring 2023" },
+        { text: "Fall 2022", value: "Fall 2022" },
+        { text: "Spring 2022", value: "Spring 2022" },
+        { text: "Fall 2021", value: "Fall 2021" },
+      ],
+      onFilter: (value: boolean | Key, record: Member) => {
+        return record.joined.includes(value as string);
+      },
     },
     {
       title: "Forest",
       dataIndex: "forest",
       key: "forest",
+      filters: [
+        { text: "Lost in the Woods", value: "Lost in the Woods" },
+        { text: "Ragtag", value: "Ragtag" },
+        { text: "Magic Tree House", value: "Magic Tree House" },
+        { text: "Howl's Moving Forest", value: "Howl's Moving Forest" },
+        { text: "Onlyfamilia", value: "Onlyfamilia" },
+      ],
+      onFilter: (value: boolean | Key, record: Member) => {
+        return record.forest.includes(value as string);
+      },
     },
     {
       title: "Tree",
