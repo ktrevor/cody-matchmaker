5c4db703b197e7d44a759299563f3cbd883a590f
alikatgullon <aliciakgullon@gmail.com>
2025-02-26 23:15:56 -0800

    saving changes to botfunctions.ts

--- src/slack/BotFunctions.ts
+++ src/slack/BotFunctions.ts
@@ -1,10 +1,10 @@
 import { getDonuts } from "../donuts/firebaseDonutFunctions";
 
 const donuts = await getDonuts();
-const donut = donuts[0]
+const donut = donuts[0];
 
 const groups = donut.groups;
 
-console.log(donuts)
-console.log(donut)
-console.log(groups)
+console.log(donuts);
+console.log(donut);
+console.log(groups);
