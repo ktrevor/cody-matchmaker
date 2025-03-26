0b7d489794e286ae6700b00ba7789859c61da994
ktrevor <katestrevor@gmail.com>
2025-03-26 00:45:50 -0700

    pushing without token

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 import { WebClient } from "@slack/web-api";
-const token = "xoxb-8651876146358-8661268937315-db1T3DX2IbGI23aIpyhYNmSl";
+const token = "";
 const web = new WebClient(token);
 
 export default async function handler(req, res) {
