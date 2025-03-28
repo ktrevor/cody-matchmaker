44afc6c840e3d1587603c19215f823257b1d26bd
ktrevor <katestrevor@gmail.com>
2025-03-27 20:59:24 -0700

    fixing batch size

--- api/createSlackGroups.js
+++ api/createSlackGroups.js
@@ -64,6 +64,7 @@ export default async function handler(req, res) {
       }
     };
 
+    console.log("processing batch");
     await processBatch(currentBatch, currentBatch + batchSize);
 
     res.status(200).json({ ok: true, createdGroups });
